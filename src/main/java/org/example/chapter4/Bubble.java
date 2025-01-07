07de5bd791f83ecf3cef964453074892a5619267
yuliaminkh <yuliaminikh@gmail.com>
2025-01-07 12:46:46 +0300

    class in help

---
+++ src/main/java/org/example/chapter4/Bubble.java
@@ -0,0 +1,35 @@
+package org.example.chapter4;
+
+public class Bubble {
+    public static void main(String args[]) {
+        int nums[] = {99, -10, 100123, 18, -978, 5623, 463, -9, 287, 49 };
+        int a, b, t;
+        int size;
+
+        size = 10;
+
+        // Отобразить исходный массив
+        System.out.print("Исходный массив: ");
+        for (int i = 0; i < size; i++) {
+            System.out.print(nums[i] + " ");
+        }
+        System.out.println();
+
+        // Реализовать алгоритм пузырьковой сортировки
+        for (a = 1; a < size; a++) {
+            for (b = size - 1; b >= a; b--) {
+                if (nums[b-1] > nums[b]) {
+                    t = nums[b-1];
+                    nums[b-1] = nums[b];
+                    nums[b] = t;
+                }
+            }
+        }
+
+        System.out.print("Отсортированный массив: ");
+        for (int i = 0; i < size; i++) {
+            System.out.print(nums[i] + " ");
+        }
+        System.out.println();
+    }
+}
