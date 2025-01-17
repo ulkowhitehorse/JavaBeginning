79b765a1f7d18c8c0ef194e0f17e53340e7a442c
yuliaminkh <yuliaminikh@gmail.com>
2025-01-17 12:46:33 +0300

    Расширение ключа шифрования на строку из 8ми символов

--- src/main/java/org/example/chapter4/Crypto.java
+++ src/main/java/org/example/chapter4/Crypto.java
@@ -2,16 +2,36 @@ package org.example.chapter4;
 
 public class Crypto {
     private int key;
+    private String keyString;
 
     public Crypto(int key){
         this.key = key;
+        System.out.println("Ключ шифрования key = " + this.key);
+
+    }
+
+    public Crypto(String keyString) {
+        // Добавить ключ keyString, если введена строка
+        if(keyString.length() == 8) {
+            this.keyString = keyString;
+            System.out.println("Ключ шифрования keyString = " + this.keyString);
+            for(int i=0; i < keyString.length(); i++) {
+                key += keyString.charAt(i);
+            }
+            key /= keyString.length();
+            System.out.println("Ключ шифрования равен: " + key);
+        } else {
+            this.keyString = "0";
+            key = 88;
+            System.out.println("Данная строка некорректна. Введенная строка должна состоять из 8ми символов. ");
+            System.out.println("Ключ шифрования key задан по умолчанию: " + key);
+        }
     }
 
     public String encode(String msg) {
         String encmsg = "";
 
-        //Зашифровать сообщение
-        for(int i = 0; i < msg.length(); i++){
+        for (int i = 0; i < msg.length(); i++) {
             // Построение зашифрованной строки сообщения
             encmsg = encmsg + (char) (msg.charAt(i) ^ key);
         }
@@ -38,7 +58,7 @@ public class Crypto {
         System.out.print("Исходное сообщение: ");
         System.out.println(msg);
 
-        Crypto crypto = new Crypto(88);
+        Crypto crypto = new Crypto("Password");
 
         // Зашифровать сообщение
         encmsg = crypto.encode(msg);
