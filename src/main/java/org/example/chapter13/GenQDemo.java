4a1b17ac92c860ea8c9ebbb3d0cdff9149cb4d87
yuliaminkh <yuliaminikh@gmail.com>
2025-06-03 09:01:04 +0300

    genQueue<T>

--- src/main/java/org/example/chapter13/GenQDemo.java
+++ src/main/java/org/example/chapter13/GenQDemo.java
@@ -5,15 +5,55 @@ public class GenQDemo {
         String[] strArray = new String[10];
         GenQueue<String> strQ = new GenQueue<String>(strArray);
 
+        // Создать очередь для хранения целых чисел
+        Integer[] intMassive = new Integer[10];
+        GenQueue<Integer> intQueue = new GenQueue<Integer>(intMassive);
+
+        Integer iVal;
+
+        System.out.println("Демонстрация очереди чисел типа Integer");
         try {
-            strQ.put("l");
+            for (int i = 0; i < 5; i++) {
+                System.out.println("Добавление " + i + " в очередь intQueue");
+                intQueue.put(i);
+            }
+        } catch (QueueFullException exc) {
+            System.out.println(exc);
+        }
+        System.out.println();
+
+        try {
+            for (int i = 0; i < 5; i++) {
+                System.out.print("Получение следующего числа типа Integer из очереди q: ");
+                iVal = intQueue.get();
+                System.out.println(iVal);
+            }
+        } catch (QueueEmptyException exc) {
+            System.out.println(exc);
+        }
+        System.out.println();
+
+        // Создать очередь для хранения чисел с плавающей точкой
+        Double[] doubleMassive = new Double[10];
+        GenQueue<Double> doubleGenQueue = new GenQueue<>(doubleMassive);
+
+        Double dVal;
+
+        System.out.println("Демонстрация очереди чисел типа Double");
+        try {
+            for (int i = 0; i < 5; i ++ ) {
+                System.out.println("Добавление " + (double) i/2 + " в очередь doubleGenQueue");
+                doubleGenQueue.put((double) i / 2);
+            }
         } catch (QueueFullException exc) {
             System.out.println(exc);
         }
 
         try {
-            System.out.println(strQ.get());
-            System.out.println(strQ.get());
+            for (int i = 0; i < 5; i++) {
+                System.out.print("Получение следующего числа типа Double из очереди doubleGenQueue: ");
+                System.out.println(doubleGenQueue.get());
+            }
         } catch (QueueEmptyException exc) {
             System.out.println(exc);
         }
